f633c4bd8b100f2a4d30e769d69a9fcc90df7ce4
SergiuChis <sergiuchis2k@gmail.com>
2022-04-18 08:08:47 +0300

    lab4 take home

--- src/test/java/ssvv/example/IntegrationTesting.java
+++ src/test/java/ssvv/example/IntegrationTesting.java
@@ -28,21 +28,18 @@ public class IntegrationTesting {
     NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
     Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
 
-    @Test
     void testShortName() {
         var student = new Student("1", "a", 932, "george@ubbcluj.ro");
 
         Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
     }
 
-    @Test
     void testIdNotNull() {
         var tema = new Tema(null, "descriere 2",5, 6);
 
         Assertions.assertThrows(ValidationException.class, () -> service.addTema(tema));
     }
 
-    @Test
     void testGradeOutsideBoundaries() {
         LocalDate ld = LocalDate.of(2022, 1, 1);
         var grade = new Nota("455", "1003", "2", 15, ld);
